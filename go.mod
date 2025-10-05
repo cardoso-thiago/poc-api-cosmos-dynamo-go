9ffa56a66e110d96ab3e94f9d9e8dd96f0bb092d
cardoso-thiago <thiago.cardoso33@gmail.com>
2025-10-05 09:06:24 -0300

    Upgrade da sdk AWS e integração com otel AWS

--- go.mod
+++ go.mod
@@ -4,7 +4,12 @@ go 1.25.1
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos v1.4.1
-	github.com/aws/aws-sdk-go v1.51.0
+	github.com/aws/aws-sdk-go-v2 v1.39.2
+	github.com/aws/aws-sdk-go-v2/config v1.31.12
+	github.com/aws/aws-sdk-go-v2/credentials v1.18.16
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.20.14
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.51.0
+	go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-sdk-go-v2/otelaws v0.63.0
 	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.63.0
 	go.opentelemetry.io/otel v1.38.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.38.0
@@ -13,6 +18,20 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.31.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.11.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sns v1.38.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.42.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.29.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.35.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.38.6 // indirect
+	github.com/aws/smithy-go v1.23.0 // indirect
 	github.com/bytedance/sonic v1.14.0 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cenkalti/backoff/v5 v5.0.3 // indirect
@@ -28,7 +47,6 @@ require (
 	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.27.2 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- repository/dynamo.go
+++ repository/dynamo.go
@@ -2,59 +2,60 @@ package repository
 
 import (
 	"context"
-	"encoding/json"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/cardoso-thiago/poc-api-cosmos-dynamo-go/models"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-sdk-go-v2/otelaws"
 )
 
 type DynamoRepository struct {
-	db        *dynamodb.DynamoDB
+	db        *dynamodb.Client
 	tableName string
 }
 
 func NewDynamoRepository(endpoint, region, tableName string) (*DynamoRepository, error) {
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String(region),
-		Endpoint:    aws.String(endpoint),
-		Credentials: credentials.NewStaticCredentials("dummy", "dummy", ""),
-	})
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(region),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("dummy", "dummy", "")),
+	)
 	if err != nil {
 		return nil, err
 	}
-	db := dynamodb.New(sess)
-	return &DynamoRepository{db: db, tableName: tableName}, nil
+
+	otelaws.AppendMiddlewares(&cfg.APIOptions)
+
+	client := dynamodb.NewFromConfig(cfg, func(o *dynamodb.Options) {
+		o.BaseEndpoint = aws.String(endpoint)
+	})
+
+	return &DynamoRepository{db: client, tableName: tableName}, nil
 }
 
 func (r *DynamoRepository) GetItem(ctx context.Context, id string) (*models.Relationship, error) {
-	result, err := r.db.GetItemWithContext(ctx, &dynamodb.GetItemInput{
-		TableName: aws.String(r.tableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"id": {S: aws.String(id)},
+	out, err := r.db.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: &r.tableName,
+		Key: map[string]types.AttributeValue{
+			"id": &types.AttributeValueMemberS{Value: id},
 		},
 	})
 	if err != nil {
 		return nil, err
 	}
-	if result.Item == nil {
+
+	if out.Item == nil {
 		return nil, os.ErrNotExist
 	}
+
 	var rel models.Relationship
-	b, err := json.Marshal(result.Item)
-	if err != nil {
+	if err := attributevalue.UnmarshalMap(out.Item, &rel); err != nil {
 		return nil, err
 	}
-	m := map[string]interface{}{}
-	if err := json.Unmarshal(b, &m); err != nil {
-		return nil, err
-	}
-	if err := dynamodbattribute.UnmarshalMap(result.Item, &rel); err != nil {
-		return nil, err
-	}
+
 	return &rel, nil
 }
